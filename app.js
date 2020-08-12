ba8c96eca0b861a2e9ced7ad015c7adfebea1ee2
sai-myne <ahlusir@gmail.com>
2020-08-12 12:43:03 +0800

    mongoose

--- app.js
+++ app.js
@@ -1,18 +1,20 @@
 const express = require('express');
 const morgan = require('morgan');
-
+const mongoose = require('mongoose');
 
 // express app
 const app = express();
 
 // connect to mongodb
 const dbURI = 'mongodb+srv://netninja:test1234@nodetuts.pebxw.mongodb.net/<dbname>?retryWrites=true&w=majority';
+mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
+    .then(result => app.listen(3000))
+    .catch(err => console.log(err));
 
 // register view engine
 app.set('view engine', 'ejs');
 
-// listen for requests
-app.listen(3000);
+
 
 // middleware & static files
 app.use(express.static('public'))
