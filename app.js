5ced36379af5de60b21ce2984030e316ddc65333
sai-myne <ahlusir@gmail.com>
2020-08-12 10:51:12 +0800

    ejs view engine

--- app.js
+++ app.js
@@ -3,17 +3,20 @@ const express = require('express');
 // express app
 const app = express();
 
+// register view engine
+app.set('view engine', 'ejs');
+
 // listen for requests
 app.listen(3000);
 
 app.get('/', (req, res) => {
     // res.send('<p>home page</p>');
-    res.sendFile('./views/index.html', { root: __dirname });
+    res.render('index');
 });
 
 app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
-    res.sendFile('./views/about.html', { root: __dirname });
+    res.render('about');
 });
 
 // redirects
@@ -21,7 +24,11 @@ app.get('/about-us', (req, res) => {
     res.redirect('/about');
 });
 
+app.get('/blogs/create', (req, res) => {
+    res.render('create');
+})
+
 // 404 page
 app.use((req, res) => {
-    res.sendFile('./views/404.html', { root: __dirname });
+    res.status(404).render('404');
 });
