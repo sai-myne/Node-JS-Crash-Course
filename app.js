b97d40221cdd1347dcb02b9d079d8e0b35743257
sai-myne <ahlusir@gmail.com>
2020-08-12 10:37:41 +0800

    Redirect & 404 page

--- app.js
+++ app.js
@@ -15,3 +15,13 @@ app.get('/about', (req, res) => {
     // res.send('<p>about page</p>');
     res.sendFile('./views/about.html', { root: __dirname });
 });
+
+// redirects
+app.get('/about-us', (req, res) => {
+    res.redirect('/about');
+});
+
+// 404 page
+app.use((req, res) => {
+    res.sendFile('./views/404.html', { root: __dirname });
+});
