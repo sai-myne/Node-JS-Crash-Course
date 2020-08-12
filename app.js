7d0d3f055c65bb8f7b82699361ec2b31c576e4de
sai-myne <52918521+sai-myne@users.noreply.github.com>
2020-08-12 16:15:24 +0800

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose');
 const app = express();
 
 // connect to mongodb
-const dbURI = 'mongodb+srv://netninja:test1234@nodetuts.pebxw.mongodb.net/<dbname>?retryWrites=true&w=majority';
+const dbURI = '';
 mongoose.connect(dbURI, {useNewUrlParser: true, useUnifiedTopology: true})
     .then(result => app.listen(3000))
     .catch(err => console.log(err));
