d98d73f7cebae31eb2cfa94da9f69a5de5aea2eb
sai-myne <ahlusir@gmail.com>
2020-08-12 09:57:20 +0800

    status code

--- server.js
+++ server.js
@@ -11,12 +11,15 @@ const server = http.createServer((req, res) => {
     switch(req.url){
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case '/about':
             path += 'about.html';
+            res.statusCode = 200;
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
     }
 
@@ -26,7 +29,7 @@ const server = http.createServer((req, res) => {
             console.log(err);
             res.end();
         } else {
-            // res.write(data);
+            // res.write(data);            
             res.end(data);
         }
     });
